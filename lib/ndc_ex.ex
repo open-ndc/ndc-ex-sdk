a776a6805d6b751d02f37d519117712f31665085
zaali kavelashvili <zkavalashvili@sevaa.com>
2015-12-14 19:53:37 +0400

    initial commit

--- lib/ndc_ex.ex
+++ lib/ndc_ex.ex
@@ -1,2 +1,20 @@
 defmodule NDCEx do
+  require Record
+  Record.defrecord :xmlElement, Record.extract(:xmlElement, from_lib: "xmerl/include/xmerl.hrl")
+  Record.defrecord :xmlText,    Record.extract(:xmlText,    from_lib: "xmerl/include/xmerl.hrl")
+
+  def parse do
+    file_name = 'requests/Athena/OneWay/AirShoppingRQ - ARN-LHR OneWay with one pax.xml'
+    File.read!(file_name)
+      |> scan_text
+      |> parse_xml
+  end
+
+  def scan_text(file_content) do
+    :xmerl_scan.string(String.to_char_list(file_content))
+  end
+
+  def parse_xml({xml, _}) do
+    NDCEx.AirShoppingRQ.yield_core_query(xml)
+  end
 end
