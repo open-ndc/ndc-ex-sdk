e7d62ddf0c79541fe28dbf33653482e46c7962a6
zaali kavelashvili <zkavalashvili@sevaa.com>
2015-12-16 00:08:48 +0400

    Add helper module which parses xmlNodes

---
+++ lib/base.ex
@@ -0,0 +1,36 @@
+defmodule NDCEx.Base do
+  require Record
+  Record.defrecord :xmlElement, Record.extract(:xmlElement, from_lib: "xmerl/include/xmerl.hrl")
+  Record.defrecord :xmlText,    Record.extract(:xmlText,    from_lib: "xmerl/include/xmerl.hrl")
+
+  defp parse(node) do
+    cond do
+      Record.is_record(node, :xmlElement) ->
+        name    = xmlElement(node, :name)
+        content = xmlElement(node, :content)
+        Map.put(%{}, name, parse(content))
+
+      Record.is_record(node, :xmlText) ->
+        xmlText(node, :value) |> to_string
+
+      is_list(node) ->
+        case Enum.map(node, &(parse(&1))) do
+          [text_content] when is_binary(text_content) ->
+            text_content
+
+          elements ->
+            Enum.reduce(elements, %{}, fn(x, acc) ->
+              if is_map(x) do
+                Map.merge(acc, x)
+              else
+                acc
+              end
+            end)
+        end
+
+      true -> "Unable to parse #{inspect node}"
+    end
+  end
+
+  def yeild(elements), do: Enum.map(elements, &(parse(xmlElement(&1, :content))))
+end
