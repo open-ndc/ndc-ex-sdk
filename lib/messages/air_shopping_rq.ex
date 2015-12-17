eb56825fd0473bc722918a35630fdf1d2a621663
zaali kavelashvili <zkavalashvili@sevaa.com>
2015-12-17 22:18:06 +0400

    Rename from yield_core_query to  yield. Makes easy to call functoin dinamically

--- lib/messages/air_shopping_rq.ex
+++ lib/messages/air_shopping_rq.ex
@@ -1,7 +1,36 @@
 defmodule NDCEx.Messages.AirShoppingRQ do
-  alias NDCEx.Base
-	
-  def yield_core_query(doc) do
-    'CoreQuery' |> :xmerl_xpath.string(doc) |> Base.yeild
+  #alias NDCEx.Base
+  import SweetXml
+
+  def yield(doc) do
+    #data = 'CoreQuery' |> :xmerl_xpath.string(doc) |> Base.yeild
+    doc |> xpath(~x"//CoreQuery/OriginDestinations/OriginDestination"l)
+
+    #doc
+    #|> xpath(~x"//CoreQuery"l)
+    #|> Enum.map fn (od_node) ->
+      #%{
+        #departurename: od_node |> xpath(~x"./Departure/AirportCode/text()"),
+        #arrival: od_node |> xpath(~x"./Arrival/AirportCode/text()")
+      #}
+    #end
+
+    doc
+    |> xpath(~x"//CoreQuery/OriginDestinations/OriginDestination"l,
+      departure: [
+        ~x"./Departure",
+        airport_code: ~x"./AirportCode/text()",
+        date: ~x"./Date/text()"
+      ],
+      arrival: [
+        ~x"./Arrival",
+        airport_code: ~x"./AirportCode/text()"
+      ],
+      marketing_carrier_airline: [
+        ~x"./MarketingCarrierAirline",
+        airline_id: ~x"./AirlineID/text()",
+        name: ~x"./Name/text()"
+      ]
+    )
   end
 end
