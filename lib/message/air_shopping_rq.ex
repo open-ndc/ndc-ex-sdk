6218e40cb5315be79aeded5036496ee31ac3b12e
zaali kavelashvili <zkavalashvili@sevaa.com>
2015-12-21 18:18:33 +0400

    Remove requests

--- lib/message/air_shopping_rq.ex
+++ lib/message/air_shopping_rq.ex
@@ -8,15 +8,17 @@ defmodule NDCEx.Message.AirShoppingRQ do
   def yield_core_query(params) do
 		element(:CoreQuery, [
 			element(:OriginDestinations, [
-				element(:OriginDestination, [ 
-					element(:Departure, [ 
-						element(:AirportCode, params[:CoreQuery][:OriginDestinations][:OriginDestination][:Departure][:AirportCode]),
-						element(:Date, params[:CoreQuery][:OriginDestinations][:OriginDestination][:Departure][:Date])
+				element(:OriginDestination, [
+					element(:Departure, [
+            element(:AirportCode, params[:CoreQuery][:OriginDestinations][:OriginDestination][:Departure][:AirportCode]),
+            element(:Date, params[:CoreQuery][:OriginDestinations][:OriginDestination][:Departure][:Date])
+						#AirportCode: params[:CoreQuery][:OriginDestinations][:OriginDestination][:Departure][:AirportCode],
+						#Date: params[:CoreQuery][:OriginDestinations][:OriginDestination][:Departure][:Date]
           ]),
-					element(:Arrival, [ 
+					element(:Arrival, [
 						element(:AirportCode, params[:CoreQuery][:OriginDestinations][:OriginDestination][:Departure][:AirportCode])
           ]),
-					element(:MarketingCarrierAirline, [ 
+					element(:MarketingCarrierAirline, [
 						element(:AirlineID, "9A"),
 						element(:Name, "Athena Air")
           ])
