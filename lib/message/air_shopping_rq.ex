de1f45c25711036c494cf32ed0c980b4f4ba1e6f
zaali kavelashvili <zkavalashvili@sevaa.com>
2015-12-26 18:37:13 +0400

    Get multiple origin_destinations

--- lib/message/air_shopping_rq.ex
+++ lib/message/air_shopping_rq.ex
@@ -1,30 +1,50 @@
 defmodule NDCEx.Message.AirShoppingRQ do
-  import XmlBuilder
+  #import XmlBuilder
+  #@query_params [ 
+                  #CoreQuery: [
+                    #OriginDestinations: [
+                      #OriginDestination: [
+                        #Departure: [
+                          #AirportCode: "MUC",
+                          #Date: "2016-04-01"
+                        #],
+                        #Arrival: [
+                          #AirportCode: "LHR"
+                        #]
+                      #]
+                    #]
+                  #]
+                #]
 
   def yield(params) do
-		yield_core_query(params)
+    #yield_core_query(params)
+    extract_params(params)
+  end
+
+  def get_origin_destinations(params) do
+    Enum.each params, fn el ->
+      [
+        element(:OriginDestination, [
+          element(:Departure, [
+            element(:AirportCode, el[:OriginDestination][:Departure][:AirportCode]),
+            element(:Date, el[:OriginDestination][:Departure][:Date])
+          ]),
+          element(:Arrival, [
+            element(:AirportCode, el[:OriginDestination][:Departure][:AirportCode])
+          ]),
+          element(:MarketingCarrierAirline, [
+            element(:AirlineID, "9A"),
+            element(:Name, "Athena Air")
+          ])
+        ])
+      ]
+    end
   end
 
   def yield_core_query(params) do
-		element(:CoreQuery, [
-			element(:OriginDestinations, [
-				element(:OriginDestination, [
-					element(:Departure, [
-            element(:AirportCode, params[:CoreQuery][:OriginDestinations][:OriginDestination][:Departure][:AirportCode]),
-            element(:Date, params[:CoreQuery][:OriginDestinations][:OriginDestination][:Departure][:Date])
-						#AirportCode: params[:CoreQuery][:OriginDestinations][:OriginDestination][:Departure][:AirportCode],
-						#Date: params[:CoreQuery][:OriginDestinations][:OriginDestination][:Departure][:Date]
-          ]),
-					element(:Arrival, [
-						element(:AirportCode, params[:CoreQuery][:OriginDestinations][:OriginDestination][:Departure][:AirportCode])
-          ]),
-					element(:MarketingCarrierAirline, [
-						element(:AirlineID, "9A"),
-						element(:Name, "Athena Air")
-          ])
-				])
-			])
-		])
+    element(:CoreQuery, [
+      element(:OriginDestinations, get_origin_destinations(params[:CoreQuery][:OriginDestinations]))
+    ])
   end
 
 end
