f454fe42dcd62d1c5737f19211879e1537fbfbfe
zaali kavelashvili <zkavalashvili@sevaa.com>
2016-01-04 02:34:55 +0400

    Send xml to rest url via HTTPotion

--- lib/ndc_ex.ex
+++ lib/ndc_ex.ex
@@ -1,4 +1,6 @@
 defmodule NDCEx do
+  require HTTPotion
+  require Logger
   @acceptable_ndc_methods [ AirShopping: ["AirShoppingRQ", "AirShoppingRS"],
                             FlightPrice: ["FlightPriceRQ", "FlightPriceRS"],
                             SeatAvailability: ["SeatAvailabilityRQ", "SeatAvailabilityRS"],
@@ -10,43 +12,34 @@ defmodule NDCEx do
                             OrderRetrieve: ["OrderRetrieveRQ", "OrderViewRS"],
                             OrderCancel: ["OrderCancelRQ", "OrderCancelRS"]
                           ]
-  @query_params [
-                  CoreQuery: [
-                    OriginDestinations: [
-                      OriginDestination: [
-                        Departure: [
-                          AirportCode: "MUC",
-                          Date: "2016-04-01"
-                        ],
-                        Arrival: [
-                          AirportCode: "LHR"
-                        ]
-                      ],
-                      OriginDestination: [
-                        Departure: [
-                          AirportCode: "MUC",
-                          Date: "2016-04-01"
-                        ],
-                        Arrival: [
-                          AirportCode: "LHR"
-                        ]
-                      ]
-                    ]
-                  ]
-                ]
 
   def request(method, params) do
-    #for testing purposes params are taken from module attribute defined above
-    #params = @query_params
-    #method = :AirShopping
-
-    ndc_config = get_mix_config(:ndc)
     [request_name, response_name] = @acceptable_ndc_methods[method]
 
-    NDCEx.Message.Base.build_document(request_name, params, ndc_config)
+    :ndc
+    |> get_mix_config
+    |> NDCEx.Message.Base.build_document(request_name, params)
+    |> rest_call_with_message(get_mix_config(:rest))
   end
 
   def get_mix_config(key) when is_atom(key) do
     Application.get_env(:ndc_ex_sdk, key)
+  end
+
+  def rest_call_with_message(xml, rest_config) do
+    #qwe = HTTPotion.post rest_config[:url], [body: xml, headers: rest_config[:headers]]
+    Logger.debug xml
+
+    case HTTPotion.post rest_config[:url], [body: xml, headers: rest_config[:headers]] do
+      %HTTPotion.Response{body: body, headers: headers, status_code: 200 } ->
+        {:ok, body}
+      %HTTPotion.Response{body: body, headers: headers, status_code: status_code } ->
+        {:error, body}
+      %HTTPotion.Response{status_code: 404} ->
+        {:error, "Not found :("}
+      _ ->
+        {:error, "Pattern matching did not worked"}
+        
+    end
   end
 end
