04bb419c1fed86c4ebd145a8dd4e52c1a113b799
zaali kavelashvili <zkavalashvili@sevaa.com>
2016-01-08 17:43:45 +0400

    Add ServiceList test

---
+++ test/service_list_test.exs
@@ -0,0 +1,18 @@
+defmodule ServiceListTest do
+  require Logger
+  use ExUnit.Case
+  require HTTPotion
+  doctest NDCEx
+
+  @params [
+    ShoppingResponseIDs: [
+      ResponseID: "RE6ac7227515a047a581d722971b9fa28f"
+      ]
+    ]
+
+  test "Call ServiceList request" do
+    data = NDCEx.request(:ServiceList, @params)
+    IO.inspect data
+    assert 1+1 == 2
+  end
+end
