a7d4f261cf02c260a597032636ee6e297edce3bd
Jorge Díaz <jorgedf@gmail.com>
2016-01-11 21:57:35 +0100

    Add basic AirShopping requests testing.

--- test/air_shopping_test.exs
+++ test/air_shopping_test.exs
@@ -1,39 +1,49 @@
 defmodule AirShoppingTest do
+
   require Logger
   use ExUnit.Case
   require HTTPotion
   doctest NDCEx
 
-
-  @core_query_params [
+  @valid_core_query_params [
                   CoreQuery: [
                     OriginDestinations: [
                       OriginDestination: [
                         Departure: [
-                          AirportCode: "MUC",
-                          Date: "2016-04-01"
+                          AirportCode: "SFX",
+                          Date: "2016-03-01"
                         ],
                         Arrival: [
-                          AirportCode: "LHR"
-                        ]
-                      ],
-                      OriginDestination: [
-                        Departure: [
-                          AirportCode: "MUC",
-                          Date: "2016-04-01"
-                        ],
-                        Arrival: [
-                          AirportCode: "LHR"
+                          AirportCode: "MAD"
                         ]
                       ]
                     ]
                   ]
                 ]
 
-  test "Call AirShopping request" do
-    data = NDCEx.request(:AirShopping, @core_query_params)
-    IO.inspect data
-    assert 1+1 == 2
+  @invalid_core_query_params [
+                  CoreQuery: [
+                    OriginDestinations: [
+                      OriginDestination: [
+                        Departure: [
+                          AirportCode: "SFX"
+                        ],
+                        Arrival: [
+                          AirportCode: "MAD"
+                        ]
+                      ]
+                    ]
+                  ]
+                ]
+
+  test "Valid AirShopping request returns :ok status" do
+    {status, _} = NDCEx.request(:AirShopping, @valid_core_query_params)
+    assert status = :ok
+  end
+
+  test "Invalid AirShopping request returns not :ok status" do
+    {status, _} = NDCEx.request(:AirShopping, @invalid_core_query_params)
+    assert status = :error
   end
 
 end
