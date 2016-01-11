2566a21597bcb1614abb8628f4e5ecaf4f49a950
Jorge Díaz <jorgedf@gmail.com>
2016-01-11 21:56:10 +0100

    Push Elixir version to 1.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NDCExSdk.Mixfile do
   def project do
     [app: :ndc_ex_sdk,
      version: "0.0.1",
-     elixir: "~> 1.1.1",
+     elixir: "~> 1.2.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
